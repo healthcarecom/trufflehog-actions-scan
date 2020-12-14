40bd7bb80747a827c3e93356f292c6c42192fca4
dporr <dporras.mail@gmail.com>
2020-12-14 11:18:24 -0600

    Redirect results to logfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-args="--regex --rules regexes.json --entropy=False --json"
+args="--regex --rules regexes.json --entropy=False"
 
 if [ -n "${INPUT_BRANCH}" ]; then
   args="${args} --branch ${INPUT_BRANCH}"
 fi
 
 cp /regexes.json .
-/usr/local/bin/trufflehog ${args} . |tee > logs.txt
+/usr/local/bin/trufflehog ${args} . > logs.txt
+cat logs.txt
 #status_code=$(echo ${PIPESTATUS[0]})
 echo "::set-output name=result::$(cat logs.txt)"
 #echo "::set-output name=status_code::$status_code"
