9f0db5085ca94381c8d61de2eed2b4a46e0990c2
dporr <dporras.mail@gmail.com>
2020-12-14 13:02:49 -0600

    Fix: Multiline strings

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,12 @@ fi
 cp /regexes.json .
 /usr/local/bin/trufflehog ${args} . > logs.txt
 #https://unix.stackexchange.com/questions/14684/removing-control-chars-including-console-codes-colours-from-script-output
-result=$(sed -i -e "s/\x1b\[.\{1,5\}m//g" logs.txt)
+result="$(sed -e "s/\x1b\[.\{1,5\}m//g" logs.txt)"
 echo $result
 #https://github.community/t/set-output-truncates-multiline-strings/16852/3
 result="${result//'%'/'%25'}"
 result="${result//$'\n'/'%0A'}"
 result="${result//$'\r'/'%0D'}"
-
+echo "$result"
 echo "::set-output name=result::$result"
 #echo "::set-output name=status_code::$status_code"
